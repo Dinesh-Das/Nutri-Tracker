0cb7a570542e942c486dce29c0d9dc9eebda646a
Dinesh <dineshdas1016@gmail.com>
2021-11-03 22:06:53 +0530

    Added Google Signin and Signout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:nutri_tracker/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+
+import 'login_screens/google_signin/google_signin.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,22 +17,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Animation',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        // define our text field style
-        inputDecorationTheme: const InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.white70,
-          border: InputBorder.none,
-          hintStyle: TextStyle(color: Colors.black),
-          contentPadding:
-              EdgeInsets.symmetric(vertical: 16 * 1.2, horizontal: 16),
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Nutri-Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          // define our text field style
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: Colors.white70,
+            border: InputBorder.none,
+            hintStyle: TextStyle(color: Colors.black),
+            contentPadding:
+                EdgeInsets.symmetric(vertical: 16 * 1.2, horizontal: 16),
+          ),
         ),
+        home: const Splash(),
       ),
-      home: const Splash(),
     );
   }
 }
