340e1913698fb9411d057fa58f8f864626e022f0
ashu <ashu.deshmukh560@gmail.com>
2021-11-26 21:21:48 +0530

    added circular indicator for Showing BMI

--- lib/screens/dietrylist/dietry_list.dart
+++ lib/screens/dietrylist/dietry_list.dart
@@ -47,7 +47,7 @@ class dietrylist extends StatelessWidget {
                                   BlendMode.multiply),
                               fit: BoxFit.cover,
                               image: NetworkImage(
-                                  "https://picsum.photos/250?image=9"),
+                                  "https://images.unsplash.com/photo-1630855907465-99267502dfbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"),
                             ),
                           ),
                         )),
